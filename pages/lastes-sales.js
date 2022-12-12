9feb1dfb7338aefab0bded7eb0c5a5ba265339c7
TranTrungTinLV <trungtintin1989@gmail.com>
2022-12-12 12:34:34 +0700

    client_useEffect

--- pages/lastes-sales.js
+++ pages/lastes-sales.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import {useSWR} from 'swr'
+
 function LastesSales() {
     const [sales, setSales] = useState();
-    // const [isLoading, setLoading] = useState(false);
-    const {data,error} = useSWR('https://nextjs-course-27435-default-rtdb.firebaseio.com/sales.json');
+    const [isLoading, setLoading] = useState(false);
+
     useEffect(() => {
-        if(data){
-            const transformedSales = [];
+        setLoading(true);
+        fetch(
+            'https://nextjs-course-27435-default-rtdb.firebaseio.com/sales.json'
+        ).then((respone) => respone.json())
+            .then(data => {
+                const transformedSales = [];
                 for (const key in data) {
                     transformedSales.push({
                         id: key,
@@ -15,32 +19,14 @@ function LastesSales() {
                     });
                 }
                 setSales(transformedSales);
-        }
-    }, [data]);
-
-    // useEffect(() => {
-    //     setLoading(true);
-    //     fetch(
-    //         'https://nextjs-course-27435-default-rtdb.firebaseio.com/sales.json'
-    //     ).then((respone) => respone.json())
-    //         .then(data => {
-    //             const transformedSales = [];
-    //             for (const key in data) {
-    //                 transformedSales.push({
-    //                     id: key,
-    //                     username: data[key].usewmicrname,
-    //                     volume: data[key].volume
-    //                 });
-    //             }
-    //             setSales(transformedSales);
-    //             setLoading(false);
-    //         });
-    // }, []);
-    if(error) {
-        return <p>Data is not yes</p>
-    }
-    if (!data || !sales) {
+                setLoading(false);
+            });
+    }, []);
+    if (isLoading) {
         return <p>Loading...</p>;
+    }
+    if(!sales) {
+        return <p>Data is not yes</p>
     }
     return (
         <ul>
